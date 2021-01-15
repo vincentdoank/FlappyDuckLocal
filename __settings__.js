03512b938143312c68b3fcbdf177c516ef0de8c3
vincentdoank <vincent.vlntn@gmail.com>
2021-01-15 12:17:10 +0700

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "708388.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 17448558, 17448551, 17448553, 17448554, 17448552, 17448555, 17448556, 17448557, 17448559, 17448560, 17448561, 17448562, 17448563, 17448565, 17448566, 18834931, 18840242 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
